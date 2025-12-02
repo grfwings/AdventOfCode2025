f6e7b9d72fa14a3dab8e185b5d1feab2e372e3bb
Griffin Evans <griffinevans@protonmail.com>
2025-12-02 02:35:49 -0800

    part 2 unfinished

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,15 +1,69 @@
-use core::panic;
 use std::fs::File;
 use std::io::{self, Read};
 use std::env;
 
+macro_rules! dbg_println {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        {
+        println!($($arg)*);
+        }
+    }
+}
+
+fn read_file(file_path: &str) -> io::Result<String> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(contents)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut filename = "";
+    let mut run_part1 = false;
+    let mut run_part2 = false;
+
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", args[0]);
+        return;
+    }
+
+    for arg in args.iter().skip(1) {
+        match arg.as_str() {
+            "--part1" => run_part1 = true,
+            "--part2" => run_part2 = true,
+            _ => filename = arg,
+        }
+    }
+
+    if !run_part1 && !run_part2 {
+        run_part1 = true;
+        run_part2 = true;
+    }
+
+    let input = read_file(filename).expect("Unable to open input file");
+
+    if run_part1 {
+        let p1_sol = solve_p1(&input);
+        println!("The password for Part 1 is: {}", p1_sol);
+    }
+
+    if run_part2 {
+        let p2_sol = solve_p2(&input);
+        println!("The password for Part 2 is: {}", p2_sol);
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct ModInt {
+struct Dial {
     val: u32,
     modulus: u32,
 }
 
-impl ModInt {
+impl Dial {
     fn new(val: u32, modulus: u32) -> Self {
         Self {
             val: val % modulus,
@@ -27,31 +81,12 @@ impl ModInt {
     }
 }
 
-fn read_file(file_path: &str) -> io::Result<String> {
-    let mut file = File::open(file_path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    Ok(contents)
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        println!("Usage: {} <filename>", args[0]);
-        return;
-    }
-
-    let filename = &args[1];
-
-    let input = read_file(filename).expect("Unable to open input file");
-
-    let mut dial = ModInt::new(50, 100);
+fn solve_p1(input: &str) -> u32 {
+    let mut dial = Dial::new(50, 100);
 
     let mut zeroes: u32 = 0;
 
-    println!("The dial starts by pointing at 50");
+    dbg_println!("The dial starts by pointing at 50");
 
     for line in input.lines() { 
         let dir: &str = &line[..1];
@@ -67,8 +102,59 @@ fn main() {
             zeroes += 1;
         }
 
-        println!("The dial is rotated {} to point at {}", line, dial.val);
+        dbg_println!("The dial is rotated {} to point at {}", line, dial.val);
     }
 
-    println!("The password is {}", zeroes);
+    zeroes
+}
+
+fn solve_p2(input: &str) -> u32 {
+    let mut dial = Dial::new(50, 100);
+
+    let mut clicks = 0;
+
+    for line in input.lines() {
+        let dir: &str = &line[..1];
+        let num: u32 = line[1..].parse().unwrap();
+
+        match dir {
+            "R" => {
+                let sav = dial.val;
+                dial.add(num);
+                if sav == 0 && num > 0 {
+                    // Starting from 0, count complete rotations
+                    clicks += num / dial.modulus;
+                } else if num > dial.modulus - sav {
+                    let rem = num - (dial.modulus - sav);
+                    clicks += 1 + rem / dial.modulus;
+                } else if dial.val == 0 && sav != 0 {
+                    clicks += 1;
+                }
+            },
+            "L" => {
+                let sav = dial.val;
+                dial.sub(num);
+
+                if sav == 0 && num > 0 {
+                    // Starting from 0, count complete rotations
+                    clicks += num / dial.modulus;
+                } else {
+                    if num > sav {
+                        let rem = num - sav;
+                        clicks +=  1 + (rem - 1) / dial.modulus;
+                    } else if dial.val == 0 && sav != 0 {
+                    clicks += 1;
+                    }
+                }
+            },
+            _ => panic!("Bad value for direction"),
+        };
+
+        assert!(dial.val < dial.modulus, "Dial value {} out of range!", dial.val);
+
+        dbg_println!("The dial is rotated {} to point at {}. Clicks: {}", line, dial.val, clicks);
+
+    }
+
+    clicks
 }
